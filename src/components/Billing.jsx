077b20b479f740f356ebd381f40a0f31753d8c26
coolakburak <hburakcolak@gmail.com>
2023-10-05 10:38:51 +0300

    major fixes

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,10 +1,49 @@
 import { apple, bill, google } from "../assets";
-import styles, {layout} from "../style";
+import styles, { layout } from "../style";
 
 const Billing = () => {
-  return <section id="product" className={layout.sectionReverse}>
-
-  </section>;
+  return (
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img
+          src={bill}
+          alt="bill"
+          className="w-[100%] h-[100%] relative z-[10]"
+        />
+        <div
+          className="absolute z-[3] -left1/2 top-0 w-[50%] h-[50%] rounded-full
+        white__gradient"
+        />
+        <div
+          className="absolute z-[0] -left1/2 bottom-0 w-[50%] h-[50%] rounded-full
+        pink__gradient"
+        />
+      </div>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your <br className="sm:block hidden" /> billing &
+          invoicing.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+          sunt ut alias, error perspiciatis esse repellat quibusdam magni
+          accusamus iusto.
+        </p>
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+          <img
+            src={apple}
+            alt="apple-store"
+            className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+          />
+          <img
+            src={google}
+            alt="google-store"
+            className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Billing;
